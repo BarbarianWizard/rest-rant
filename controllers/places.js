ae02f282f4f92ecda8c00da6ee73ef66dff12099
Kevin <aubeck79@gmail.com>
2022-07-31 11:03:07 -0500

    new places functioning

--- controllers/places.js
+++ controllers/places.js
@@ -1,10 +1,16 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
+
+router.get('/', (req, res) => {
+    res.render('places/index', { places })
+})
 
 router.get('/new', (req, res) => {
   res.render('places/new')
 })
 
-// GET /places
+/*
+// GET /places 
 router.get('/', (req, res) => {
   let places = [{
     name: 'H-Thai-ML',
@@ -28,11 +34,25 @@ router.get('/', (req, res) => {
       res.render('places/index', { places })
 
   })
+*/
   
+
   router.post('/', (req, res) => {
     console.log(req.body)
-    res.send('POST /places')
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
   })
+  
   
 
 module.exports = router

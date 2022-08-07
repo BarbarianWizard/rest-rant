6f3c11141be5318dbe00d850ceb9dea882708e90
Kevin <aubeck79@gmail.com>
2022-08-07 14:24:11 -0500

    helper function added

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -3,10 +3,27 @@ const Def = require('../default')
 
 function show (data) {
     return (
-        <Def>
-          <main className='container'>
-            <img id="showPic" src={data.place.pic}  />
-            <div>
+      <Def>
+      <main>
+        <div className="row">
+          <div className="col-sm-6">
+            <img src={data.place.pic} alt={data.place.name} />
+            <h3>
+              Located in {data.place.city}, {data.place.state}
+            </h3>
+          </div>
+          <div className="col-sm-6">
+            ...
+            <h2>
+              Description
+            </h2>
+            <h3>
+              {data.place.showEstablished()}
+            </h3>
+            <h4>
+              Serving {data.place.cuisines}
+            </h4>
+            <div className="col-sm-6 inline">
             <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
   Edit
 </a>  
@@ -14,22 +31,17 @@ function show (data) {
   <button type="submit" className="btn btn-danger">
     Delete
   </button>
-</form>     
-
-
-            </div>
-            
-
-
-            <h1>{ data.place.name }</h1>
-            <aside>Rating</aside>
-            <aside>Description</aside>
-            <footer>
-                Comments
-            </footer>
-          </main>
-        </Def>
+</form>
+</div>
+        </div>
+        </div>
+      </main>
+    </Def>
+    
     )
 }
 
 module.exports = show
+/*
+
+*/

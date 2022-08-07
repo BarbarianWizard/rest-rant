12109a24ed4977e34589efa78337f1e5d2c36e05
Kevin <aubeck79@gmail.com>
2022-08-07 13:44:03 -0500

    added seeders

---
+++ seeders/seed-places.js
@@ -0,0 +1,25 @@
+const db = require('../models')
+
+db.Place.create([{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: './public/images/thai.jpg',
+    founded: 1989
+}, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: `./public/images/coffee.jpg`,
+    founded: 2020
+}])
+.then(() => {
+    console.log('Success!')
+    process.exit()
+})
+.catch(err => {
+    console.log('Failure!', err)
+    process.exit()
+})

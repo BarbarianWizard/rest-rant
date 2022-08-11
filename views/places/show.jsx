98cb4972d07fe0bcc30d894c2a9ae0b3617ff7e2
Kevin <aubeck79@gmail.com>
2022-08-10 20:05:12 -0500

    comments generating

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,6 +2,25 @@ const React = require('react')
 const Def = require('../default')
 
 function show (data) {
+  let comments = (
+    <h3 className="inactive">
+      No comments yet!
+    </h3>
+  )
+  if (data.place.comments.length) {
+    comments = data.place.comments.map(c => {
+      return (
+        <div className="border">
+          <h2 className="rant">{c.rant ? 'Rant! ðŸ˜¡' : 'Rave! ðŸ˜»'}</h2>
+          <h4>{c.content}</h4>
+          <h3>
+            <stong>- {c.author}</stong>
+          </h3>
+          <h4>Rating: {c.stars}</h4>
+        </div>
+      )
+    })
+  }
     return (
       <Def>
       <main>
@@ -40,7 +59,8 @@ function show (data) {
 </div>
 <hr></hr>
 <h2>Comments</h2>
-<h3 id="greyedOut">No Comments yet!</h3>
+{comments}
+
         </div>
       </main>
     </Def>
@@ -50,5 +70,30 @@ function show (data) {
 
 module.exports = show
 /*
+<h3 id="greyedOut">No Comments yet!</h3>
+
+function show (data) {
+    let comments = (
+      <h3 className="inactive">
+        No comments yet!
+      </h3>
+    )
+    
+    return (
+        <Def>
+          <main>
+            <div className="row">
+              ...
+            </div>
+            <hr />
+            <h2>Comments</h2>
+            {comments}
+          </main>
+        </Def>
+    )
+}
+
+module.exports = show
+
 
 */

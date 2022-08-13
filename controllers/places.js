8e5d95645668544f3edd5016eb464f91cc06ca3a
Kevin <aubeck79@gmail.com>
2022-08-13 15:06:02 -0500

    Comments delete working!

--- controllers/places.js
+++ controllers/places.js
@@ -38,17 +38,6 @@ router.post('/', (req, res) => {
   })
 })
 
-router.delete('/:id', (req, res) => {
-  db.Place.findByIdAndDelete(req.params.id)
-  .then(place => {
-      res.redirect('/places')
-  })
-  .catch(err => {
-      console.log('err', err)
-      res.render('error404')
-  })
-})
-
 router.get('/new', (req, res) => {
   res.render('places/new')
 })
@@ -66,7 +55,6 @@ router.get('/:id', (req, res) => {
   })
 })
 
-
 router.put('/:id', (req, res) => {
   db.Place.findByIdAndUpdate(req.params.id, req.body)
   .then(() => {
@@ -78,7 +66,16 @@ router.put('/:id', (req, res) => {
   })
 })
 
-
+router.delete('/:id', (req, res) => {
+  db.Place.findByIdAndDelete(req.params.id)
+  .then(place => {
+      res.redirect('/places')
+  })
+  .catch(err => {
+      console.log('err', err)
+      res.render('error404')
+  })
+})
 
 
 router.get('/:id/edit', (req, res) => {
@@ -91,10 +88,6 @@ router.get('/:id/edit', (req, res) => {
   })
 })
 
-
-router.post('/:id/rant', (req, res) => {
-  res.send('GET /places/:id/rant stub')
-})
 
 router.post('/:id/comment', (req, res) => {
   console.log('post comment' , req.body)
@@ -119,7 +112,17 @@ router.post('/:id/comment', (req, res) => {
   })
 })
 
-
+router.delete('/:id/comment/:commentId', (req, res) => {
+  db.Comment.findByIdAndDelete(req.params.commentId)
+  .then(() => {
+    console.log('Success')
+    res.redirect(`/places/${req.params.id}`)
+  })
+  .catch(err => {
+    console.log('err', err)
+    res.render('error404')
+  })
+})
 
 
 module.exports = router
